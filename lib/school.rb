6fcfc3686d21dbce968477aea8f338f2c3706acb
Daniel Bovington <d.bovington@gmail.com>
2019-07-10 15:35:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_reader :roster
+
+	def initialize(name)
+	  @name = name
+		@roster = {}
+	end
+
+	def add_student(name, grade)
+		@roster[grade] ||= []
+		@roster[grade] << name
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		sorted_hash = {}
+		@roster.map { |grade, name| sorted_hash[grade] = name.sort  }
+		sorted_hash
+	end
+
+end
